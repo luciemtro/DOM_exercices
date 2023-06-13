100d27b002e38321cab23f4a72aea944fc33f7c9
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-13 10:22:38 +0200

    Fini introduction

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,2 +1,10 @@
-/*
-*/
+const titleHead = document.getElementsByTagName('title').innerText = 'Modifying the DOM';
+console.log(titleHead)
+
+const body = document.body
+
+const bodyChild = body.childNodes ;
+for (i = 0 ; i < bodyChild.length ; i++){
+    console.log(bodyChild[i])
+}
+body.style.background ='rgb(220, 80, 152)'
