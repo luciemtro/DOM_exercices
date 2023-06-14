5f2a90e8caee5169212e803ad36acf1f554aef5a
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-14 13:37:21 +0200

    4 fini

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -1,2 +1,24 @@
-/*
-*/
+let listOl = document.querySelector('ol');
+let lastchild = listOl.children[4];
+let firstchild = listOl.children[0];
+firstchild.prepend(lastchild);
+//Tâche 2
+
+const h2n2 = document.getElementsByTagName('h2')[1];
+const h2n3 = document.getElementsByTagName('h2')[2];
+
+const section2 = document.getElementsByTagName('section')[1];
+const section3 = document.getElementsByTagName('section')[2];
+
+section2.appendChild(h2n3)
+section3.appendChild(h2n2)
+
+section3.remove()
+
+
+
+
+
+
+
+
